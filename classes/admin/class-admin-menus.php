50dee1ece60b02fecbb308e100c19e80d2d42ccb
Tung Du <dinhtungdu@gmail.com>
2018-12-25 18:58:51 +0700

    As #516 Fixed all source files with phpcbf Applied WordPress Coding Standard

--- classes/admin/class-admin-menus.php
+++ classes/admin/class-admin-menus.php
@@ -18,13 +18,13 @@ class WCVendors_Admin_Menus {
 	/**
 	 * Constructor
 	 */
-	public function __construct(){
+	public function __construct() {
 
 		// Add menus
 		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
 		add_action( 'admin_menu', array( $this, 'commissions_menu' ), 50 );
 		add_action( 'admin_menu', array( $this, 'settings_menu' ), 70 );
-		add_action( 'admin_menu', array( $this, 'extensions_menu'), 80 );
+		add_action( 'admin_menu', array( $this, 'extensions_menu' ), 80 );
 		add_action( 'admin_head', array( $this, 'commission_table_header_styles' ) );
 
 		add_filter( 'set-screen-option', array( __CLASS__, 'set_commissions_screen' ), 10, 3 );
@@ -42,35 +42,52 @@ class WCVendors_Admin_Menus {
 			$menu[] = array( '', 'read', 'separator-woocommerce', '', 'wp-menu-separator wcvendors' );
 		}
 
-		add_menu_page( __( 'WC Vendors', 'wc-vendors' ), __( 'WC Vendors', 'wc-vendors' ), 'manage_woocommerce', 'wc-vendors', array( $this, 'extensions_page' ), 'dashicons-cart', '50'  );
+		add_menu_page(
+			__( 'WC Vendors', 'wc-vendors' ), __( 'WC Vendors', 'wc-vendors' ), 'manage_woocommerce', 'wc-vendors', array(
+			$this,
+			'extensions_page',
+		), 'dashicons-cart', '50'
+		);
 	}
 
 	/**
 	 * Addons menu item.
 	 */
 	public function extensions_menu() {
-		add_submenu_page( 'wc-vendors', __( 'WC Vendors Extensions', 'wc-vendors' ), __( 'Extensions', 'wc-vendors' ), 'manage_woocommerce', 'wcv-extensions', array( $this, 'extensions_page' ) );
+
+		add_submenu_page(
+			'wc-vendors', __( 'WC Vendors Extensions', 'wc-vendors' ), __( 'Extensions', 'wc-vendors' ), 'manage_woocommerce', 'wcv-extensions', array(
+				$this,
+				'extensions_page',
+			)
+		);
 		remove_submenu_page( 'wc-vendors', 'wc-vendors' );
 	}
 
 	/**
-	* 	Addons Page
-	*/
-	public function extensions_page(){
+	 *    Addons Page
+	 */
+	public function extensions_page() {
+
 		WCVendors_Admin_Extensions::output();
 	}
 
 	/**
 	 * Add the commissions sub menu
 	 *
-	 * @since 1.0.0
+	 * @since  1.0.0
 	 * @access public
-	 *
 	 */
 	public function commissions_menu() {
 
-		$commissions_page = add_submenu_page( 'wc-vendors', __( 'Commissions', 'wc-vendors' ), __( 'Commissions', 'wc-vendors' ), 'manage_woocommerce', 'wcv-commissions', array( $this, 'commissions_page' ) );\
-		add_action( "load-$commissions_page", array( $this, 'commission_screen_options' ) );
+		$commissions_page = add_submenu_page(
+			'wc-vendors', __( 'Commissions', 'wc-vendors' ), __( 'Commissions', 'wc-vendors' ), 'manage_woocommerce', 'wcv-commissions', array(
+				$this,
+				'commissions_page',
+			)
+		);
+		\
+			add_action( "load-$commissions_page", array( $this, 'commission_screen_options' ) );
 
 	} // commissions_menu()
 
@@ -78,9 +95,15 @@ class WCVendors_Admin_Menus {
 	/**
 	 * Settings menu item
 	 */
-	public function settings_menu(){
-		$settings_page = add_submenu_page( 'wc-vendors', __( 'WC Vendors Settings', 'wcvendors' ),  __( 'Settings', 'wcvendors' ), 'manage_woocommerce', 'wcv-settings', array( $this, 'settings_page' ) );
- 		add_action( 'load-' . $settings_page, array( $this, 'settings_page_init') );
+	public function settings_menu() {
+
+		$settings_page = add_submenu_page(
+			'wc-vendors', __( 'WC Vendors Settings', 'wcvendors' ), __( 'Settings', 'wcvendors' ), 'manage_woocommerce', 'wcv-settings', array(
+				$this,
+				'settings_page',
+			)
+		);
+		add_action( 'load-' . $settings_page, array( $this, 'settings_page_init' ) );
 	}
 
 
@@ -116,17 +139,19 @@ class WCVendors_Admin_Menus {
 	/**
 	 * Settings Page
 	 */
-	public function settings_page(){
+	public function settings_page() {
+
 		WCVendors_Admin_Settings::output();
 	}
 
 	/**
-	* Commission page output
-	*
-	* @since 2.0.0
-	*/
-	public function commissions_page(){
-		include( WCV_ABSPATH_ADMIN . 'views/html-admin-commission-page.php' );
+	 * Commission page output
+	 *
+	 * @since 2.0.0
+	 */
+	public function commissions_page() {
+
+		include WCV_ABSPATH_ADMIN . 'views/html-admin-commission-page.php';
 	}
 
 
@@ -139,7 +164,7 @@ class WCVendors_Admin_Menus {
 		$args   = [
 			'label'   => 'Commissions',
 			'default' => 10,
-			'option'  => 'commissions_per_page'
+			'option'  => 'commissions_per_page',
 		];
 
 		add_screen_option( $option, $args );
@@ -148,6 +173,7 @@ class WCVendors_Admin_Menus {
 	}
 
 	public static function set_commissions_screen( $status, $option, $value ) {
+
 		return $value;
 	}
 
@@ -156,26 +182,26 @@ class WCVendors_Admin_Menus {
 	 */
 	public function commission_table_header_styles() {
 
-	    $page = ( isset( $_GET[ 'page' ] ) ) ? esc_attr( $_GET[ 'page' ] ) : false;
+		$page = ( isset( $_GET['page'] ) ) ? esc_attr( $_GET['page'] ) : false;
 
+		wp_enqueue_style( 'wcv-admin-styles', wcv_assets_url . 'css/wcv-admin.css', array(), WCV_VERSION );
 
-	    wp_enqueue_style( 'wcv-admin-styles', wcv_assets_url . 'css/wcv-admin.css', array(), WCV_VERSION );
+		// Only load the styles on the license table page
+		if ( 'wcv_admin_commissions' !== $page ) {
+			return;
+		}
 
-	    // Only load the styles on the license table page
-
-	    if ( 'wcv_admin_commissions' !== $page ) return;
-
-	    echo '<style type="text/css">';
-	    echo '.wp-list-table .column-product_id { width: 20%; }';
-	    echo '.wp-list-table .column-vendor_id { width: 15%; }';
-	    echo '.wp-list-table .column-order_id { width: 8%; }';
-	    echo '.wp-list-table .column-total_due { width: 10%;}';
-	    echo '.wp-list-table .column-total_shipping { width: 10%;}';
-	    echo '.wp-list-table .column-tax { width: 10%;}';
-	    echo '.wp-list-table .column-totals { width: 10%;}';
-	    echo '.wp-list-table .column-status { width: 5%;}';
-	    echo '.wp-list-table .column-time { width: 10%;}';
-	    echo '</style>';
+		echo '<style type="text/css">';
+		echo '.wp-list-table .column-product_id { width: 20%; }';
+		echo '.wp-list-table .column-vendor_id { width: 15%; }';
+		echo '.wp-list-table .column-order_id { width: 8%; }';
+		echo '.wp-list-table .column-total_due { width: 10%;}';
+		echo '.wp-list-table .column-total_shipping { width: 10%;}';
+		echo '.wp-list-table .column-tax { width: 10%;}';
+		echo '.wp-list-table .column-totals { width: 10%;}';
+		echo '.wp-list-table .column-status { width: 5%;}';
+		echo '.wp-list-table .column-time { width: 10%;}';
+		echo '</style>';
 
 	} //table_header_styles()
 
