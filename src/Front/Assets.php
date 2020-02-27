3753b1bb08fce60b1e3738adf94371774105c8ec
digitalchild <digitalchild@gmail.com>
2020-02-27 23:40:59 +0700

    Added: Frontend asset class

---
+++ src/Front/Assets.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Load frontend assets
+ *
+ * @package WCVendors/Admin
+ */
+
+namespace WCVendors\Front;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * WCVendors_Admin_Assets Class.
+ */
+class Assets {
+
+	/**
+	 * Hook in tabs.
+	 */
+	public function init_hooks() {
+
+	}
+}
