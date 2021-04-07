eecd7be253a2d9bd7a05d6b746690a1ae1d0270f
Bhargav <desai.bhargav.qa@gmail.com>
2021-04-07 11:08:23 +0530

    Comments and configuration modifications

--- tests/acceptance/VendoraddingregularproductCest.php
+++ tests/acceptance/VendoraddingregularproductCest.php
@@ -14,20 +14,14 @@ class VendoraddingregularproductCest
     // tests
     public function frontpageWorks(AcceptanceTester $I)
     {
-		$I->amOnPage('/wp-admin');
-		$I->click('Products');//clicking on products
+		$I->amOnPage('/wp-admin');//Launching the WordPress Admin panel for vendor as adding products using the admin panel.
+		$I->click('Products');
 		$I->see('Search Products');
 		$I->click('#wpbody-content > div.wrap > a:nth-child(2)');//Clicking on to add a new products
-		$I->fillField('#title', 'Automated Product Two');
-		$I->click('#in-product_cat-15'); //Adding uncategorized category for automation.
+		$I->fillField('#title', 'Automated Product Two KOne');
+		$I->click('#in-product_cat-15'); //Setting uncategorized category for automation.
 		$I->fillField('#_regular_price', '200');//Setting the price for automated product.
-		//Unable to set inventory for the product.
-		//$I->click('#woocommerce-product-data > div.inside > div > ul > li.inventory_options.inventory_tab.show_if_simple.show_if_variable.show_if_grouped.show_if_external > a');
-		//$I->click('#_manage_stock');//setting checkbox to set inventory count
-		//$I->fillField('#_stock','200');
-		//$I->fillField('#_low_stock_amount', '10');
-		//$I->scrollTo(['css' => '#publish'], 20, 50);
-		//$I->executeJS('window.scrollTo(0,0);');//Want to scroll back to top of the page, in order for publish to be visible
+		$I->scrollTo('#title');
 		$I->click('#publish');
 		$I->see('Product published. View Product');
 		$I->click('View Product');
