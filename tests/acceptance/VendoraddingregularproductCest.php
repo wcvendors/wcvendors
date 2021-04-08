7bd8e3080464a743b1326a341b152193452812bf
Bhargav <desai.bhargav.qa@gmail.com>
2021-04-08 11:02:32 +0530

    changes for cart selection

--- tests/acceptance/VendoraddingregularproductCest.php
+++ tests/acceptance/VendoraddingregularproductCest.php
@@ -18,26 +18,36 @@ class VendoraddingregularproductCest
 		$I->click('Products');
 		$I->see('Add New');
 		$I->doubleClick('#wpbody-content > div.wrap > a:nth-child(2)');//Clicking on to add a new products
-		$I->fillField('#title', 'Automated Product Y2K KOne');
+		$I->fillField('#title', 'Automated Product Y2K2 J');
 		$I->click('#in-product_cat-15'); //Setting uncategorized category for automation.
-		$I->fillField('#_regular_price', '200');//Setting the price for automated product.
+		$I->fillField('#_regular_price', '201');//Setting the price for automated product.
 		$I->scrollTo('#show-settings-link');
 		$I->waitForElement('#publish', 30);
-		$I->click('#publish');
-		$I->see('Product published. View Product');
+		$I->doubleClick('#publish');
+		$I->waitForText('Product published. View Product', 30);
 		$I->click('View Product');
-		$I->see('Automated Product Y2K KOne');
+		$I->see('Automated Product Y2K2 J');
 		$I->click('My account');
 		$I->click('Log out');
 		$I->fillField('#username', 'customer1');
 		$I->fillField('#password', 'dM^gc87RPE&Osuj(EKPY)X8(');
 		$I->click('Log in');
-		$I->fillField('#woocommerce-product-search-field-0', 'Automated Product Y2K KOne');//searching for the product added by vendor.
+		$I->fillField('#woocommerce-product-search-field-0', 'Automated Product Y2K2 J');//searching for the product added by vendor.
 		$I->pressKey('#woocommerce-product-search-field-0', \Facebook\WebDriver\WebDriverKeys::ENTER);//Was difficult to find the exact syntax to pass enter key lol.
+		$I->wait(5);
+		//$I->scrollTo('#main > header > h1');//#main > div:nth-child(2) > form > select
+		$I->scrollTo('#main > div:nth-child(2) > form > select');
 		$I->click('Add to cart');
-		$I->see('has been added to your cart.');
-		$I->click('View cart');
-		$I->scrollTo('Proceed to checkout');
+		//$I->see('has been added to your cart.');
+		//$I->click('#content > div > div.woocommerce > div > a');//This is only valid for the first time when we have single product with the name
+		$I->wait(10);
+		//$I->executeJS('return $("#element").is(":focus")');
+		$I->executeJS('document.getElementByXpath("//*[@id="main"]/ul/li[1]/a[3]").click()');
+		//$I->click('View cart');
+		//$I->pressKey('Add to cart', \Facebook\WebDriver\WebDriverKeys::TAB);
+		//$I->pressKey('Add to cart', \Facebook\WebDriver\WebDriverKeys::ENTER);
+		$I->waitForElement('#post-7 > div > div > div.cart-collaterals > div.cart_totals.calculated_shipping > div > a', 30);
+		$I->scrollTo('#post-7 > div > div > div.cart-collaterals > div.cart_totals.calculated_shipping > div > a');//Navigate to the button Proceed to checkout
 		$I->click('Proceed to checkout');
 		$I->scrollTo('#billing_first_name');
 		$I->fillField('#billing_first_name', 'Customer');
