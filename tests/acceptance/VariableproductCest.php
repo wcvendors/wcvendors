bb36d47f56236f0ced2b5747fd5d48a8bb6e992a
Bhargav <desai.bhargav.qa@gmail.com>
2021-04-14 17:52:34 +0530

    variable product purchase

--- tests/acceptance/VariableproductCest.php
+++ tests/acceptance/VariableproductCest.php
@@ -26,8 +26,8 @@ class VariableproductCest
 		$I->dontSee('General');
 		$I->click('#_manage_stock');
 		$I->fillField('#_stock', '100');
-		$I->click('Variations');
-		$I->see('Before you can add a variation you need to add some variation attributes on the Attributes tab.');
+		//$I->click('Variations');
+		//$I->see('Before you can add a variation you need to add some variation attributes on the Attributes tab.');
 		$I->click('Attributes');
 		$I->click('//*[@id="product_attributes"]/div[1]/button');
 		$I->waitForText('Used for variations', 300);//Will also make sure that we have the product type set as variable.
@@ -53,12 +53,12 @@ class VariableproductCest
 		$I->acceptPopup();
 		$I->waitForText('variations do not have prices. Variations (and their attributes) that do not have prices will not be shown in your store.', 300);
 		$I->click('#variable_product_options_inner > div.woocommerce_variations.wc-metaboxes.ui-sortable > div:nth-child(2) > h3 > strong');
-		$I->waitForText('Sale price (₹) Schedule', 10);
+		$I->waitForText('Sale price (₹) Schedule', 20);
 		$I->fillField('#variable_regular_price_0', 121);
 		$I->click('#variable_product_options_inner > div.woocommerce_variations.wc-metaboxes.ui-sortable > div:nth-child(2) > h3 > strong');
 		$I->click('#postimagediv > div.postbox-header > h2');
 		$I->click('#variable_product_options_inner > div.woocommerce_variations.wc-metaboxes.ui-sortable > div:nth-child(3) > h3 > strong');
-		$I->waitForText('Sale price (₹) Schedule', 10);
+		$I->waitForText('Sale price (₹) Schedule', 20);
 		$I->fillField('#variable_regular_price_1', 51);
 		$I->click('#variable_product_options_inner > div.woocommerce_variations.wc-metaboxes.ui-sortable > div:nth-child(3) > h3 > strong');
 		$I->click('#postimagediv > div.postbox-header > h2');
@@ -70,5 +70,55 @@ class VariableproductCest
 		$I->see('Product published. View Product');
 		$I->click('View Product');
 		$I->see('Var Pro 1');
+		$I->amOnPage('/my-account');
+		$I->click('Log out');
+		//Logging as customer to purchase the product.
+		$I->fillField('#username', 'customer1');
+		$I->fillField('#password', 'dM^gc87RPE&Osuj(EKPY)X8(');
+		$I->click('Log in');
+		$I->fillField('#woocommerce-product-search-field-0', 'Var Pro 1');//searching for the product added by vendor.
+		$I->pressKey('#woocommerce-product-search-field-0', \Facebook\WebDriver\WebDriverKeys::ENTER);//Was difficult to find the exact syntax to pass enter key lol.
+		$I->wait(5);
+		$I->scrollTo('#main > div:nth-child(2) > form > select');//This will require 2 products with same name or simply run the script twice to add the product twice.
+		$I->click('Add to cart');
+		$I->wait(10);
+		$I->amOnPage('/cart');//Change in URL was the only option left
+		$I->click('Proceed to checkout');
+		$I->scrollTo('#billing_first_name');
+		$I->fillField('#billing_first_name', 'Customer');
+		$I->fillField('#billing_last_name', 'Automated One');
+		$I->scrollTo('#billing_address_1');
+		$I->fillField('#billing_address_1', 'sample billing address line');
+		$I->scrollTo('#billing_city');
+		$I->fillField('#billing_city', 'Nadiad');
+		$I->scrollTo('#billing_postcode');
+		$I->fillField('#billing_postcode', '387002');
+		$I->scrollTo('#billing_phone');
+		$I->fillField('#billing_phone', '1234567890');
+		$I->scrollTo('#billing_email');
+		$I->fillField('#billing_email', 'automation.customer.one@yopmail.com');
+		$I->wait(5);
+		$I->scrollTo('#payment > ul > li.wc_payment_method.payment_method_paypal > label > img'); //Clicking the WC Vendors Test Gateway for payment.
+		$I->executeJS('document.querySelector("#payment > ul > li.wc_payment_method.payment_method_wcvendors_test_gateway > label").click()');
+		$I->waitForText('This is a test gateway — not to be used on live sites for live transactions. Click here to visit WCVendors.com.', 20);//Make sure that the test gateway is set correct.
+		$I->executeJS('document.querySelector("#place_order").click()');
+		$I->waitForText('Order received', 300);
+		$I->see('Thank you. Your order has been received.');
+		$I->scrollTo('#post-8 > div > div > div > ul > li.woocommerce-order-overview__email.email');
+		$I->see('automation.customer.one@yopmail.com');
+		$I->amOnPage('/my-account');//Navigation to accounts page to log out.
+		$I->click('Log out');
+		//Product purchased by the customer
+		//Loggin in as Admin to complete the purchase.
+		//$I->amOnPage('/my-account');//Navigation to accounts page to log out.
+		$I->fillField('#username', 'admin');
+		$I->fillField('#password', '123456');
+		$I->click('Log in');
+		$I->amOnPage('/wp-admin');
+		$I->executeJS('document.querySelector("#toplevel_page_woocommerce > a > div.wp-menu-name").click()');
+		$I->executeJS('document.querySelector("#toplevel_page_woocommerce > ul > li:nth-child(3) > a").click()');
+		$I->fillField('#post-search-input', 'Var Pro 1');//searching for the product added by vendor.
+		$I->pressKey('#post-search-input', \Facebook\WebDriver\WebDriverKeys::ENTER);
+		$I->wait(3);		
     }
 }
