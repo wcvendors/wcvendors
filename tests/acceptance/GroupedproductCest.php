6338903b773bcab29d7c3717c9757c8bba4f0315
Bhargav <desai.bhargav.qa@gmail.com>
2021-04-16 11:31:10 +0530

    grouped product added

--- tests/acceptance/GroupedproductCest.php
+++ tests/acceptance/GroupedproductCest.php
@@ -17,12 +17,24 @@ class GroupedproductCest
     {
 		$I->amOnPage('/wp-admin/post-new.php?post_type=product');//Navigating direct to product addition form.
 		$I->see('Publish immediately');
-		$I->fillField('#title', 'Var Pro 1');
+		$I->fillField('#title', 'Grouped 1');
 		$I->scrollTo('#product_catdiv > div.postbox-header > h2');
 		$I->click('#product_cat-15 > label');
 		$I->click('#product-type');
-		$I->wait(2);//Waiting for the drop down to load correct for click
+		$I->wait(2);//Waiting for the drop down to load complete before clicking.
 		$I->click('#product-type > optgroup > option:nth-child(2)');
 		$I->dontSee('General');
+		$I->click('Linked Products');
+		$I->wait(1);
+		$I->fillField('//*[@id="linked_product_data"]/div[1]/p/span[1]/span[1]/span/ul/li/input', 'Var');
+		//$I->waitForElement('#select2-grouped_products-results > li:nth-child(1)', 30);
+		$I->wait(30);
+		$I->click('#select2-grouped_products-results > li:nth-child(1)');
+		$I->scrollTo('#title');
+		$I->doubleClick('#publish');
+		$I->scrollTo('#show-settings-link');
+		$I->waitForText('Product published. View Product', 300);
+		$I->click('View Product');
+		$I->see('Grouped 1');
     }
 }
