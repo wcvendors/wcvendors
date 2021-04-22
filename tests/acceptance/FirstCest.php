749502b59d7e756d3780ca75ad3a465316393fd7
Bhargav <desai.bhargav.qa@gmail.com>
2021-04-22 14:35:57 +0530

    Login credentials validation message

--- tests/acceptance/FirstCest.php
+++ tests/acceptance/FirstCest.php
@@ -4,12 +4,20 @@ class FirstCest
 {
     public function _before(AcceptanceTester $I)
     {
+		$I->amOnPage('/');
+        $I->see('wcvendors');
     }
 
-    // tests
+    // Login password errors validation.
     public function frontpageWorks(AcceptanceTester $I)
     {
-        $I->amOnPage('/');
-        $I->see('wcvendors');
+        $I->amOnPage('/my-account');
+		$I->see('My account');
+		$I->fillField('#username', 'admin');
+		$I->click('Log in');
+		$I->waitForText('Error: The password field is empty.', 300);
+		$I->fillField('#password', 'Invalidpassword');
+		$I->click('Log in');
+		$I->waitForText('Error: The password you entered for the username admin is incorrect. Lost your password?', 300);
     }
 }
